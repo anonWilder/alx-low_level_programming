a2c4d219de16c2c564ad2c1c87dc81462767b363
Kenechukwu <austineokafor.e@gmail.com>
2022-12-14 17:52:03 -0800

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 
-// define a function to generate the Fibonacci sequence
-void fibonacci(int *a, int *b) {
+// Define a function to generate the Fibonacci sequence
+void fibonacci(int *a, int *b)
+{
     int c = *a + *b;
     *a = *b;
     *b = c;
 }
 
-int main() {
-    // initialize the sum to 0
+int main(void)
+{
+    // Initialize the sum to 0
     int sum = 0;
 
-    // initialize the first two terms of the sequence
+    // Initialize the first two terms of the sequence
     int a = 1, b = 2;
 
-    // generate the Fibonacci sequence
-    while (b <= 4000000) {
-        // if the current term is even, add it to the sum
-        if (b % 2 == 0) {
+    // Generate the Fibonacci sequence
+    while (b <= 4000000)
+    {
+        // If the current term is even, add it to the sum
+        if (b % 2 == 0)
+        {
             sum += b;
         }
 
         fibonacci(&a, &b);
     }
 
-    // print the sum of the even-valued terms
+    // Print the sum of the even-valued terms
     printf("%d\n", sum);
 
     return 0;
